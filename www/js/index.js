64950c288561497aad4f9c2d2ed0bfc09790ae79
saepuel <43898714+saepuel@users.noreply.github.com>
2018-10-06 10:35:10 +0700

    Update index.js

--- www/js/index.js
+++ www/js/index.js
@@ -3,7 +3,7 @@ var app = {
     // Application Constructor
     initialize: function() {
     
-        var url = "https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY";
+        var url = https://api.nasa.gov/planetary/apod?api_key=82h9gy6ZfbeDByucbgrdMpFhAGiH9LJAeIbyWCs9
         
         $("#loadImage").click(function(){
             $.ajax({
